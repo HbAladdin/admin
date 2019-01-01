3e0ff251b335ceedb15f2e994442a46759893759
zhouruizhong <528441592@qq.com>
2019-01-01 17:17:23 +0800

    监控服务 配置中心

--- src/main/java/com/zrz/admin/AdminApplication.java
+++ src/main/java/com/zrz/admin/AdminApplication.java
@@ -1,9 +1,21 @@
 package com.zrz.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
+ *  服务监控
  * @author 周瑞忠
  * @date 2019/1/1 15:09
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
 public class AdminApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 }
